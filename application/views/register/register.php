621d01e691ba6fd33389cca7e4aaf9507621d815
Wichuta18 <slotsnow0301@gmail.com>
2019-04-06 19:47:31 +0700

    create register

---
+++ application/views/register/register.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="public/css/bootstrap/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="public/asset/animate/animate.css">
+	
+	<link rel="stylesheet" type="text/css" href="public/asset/css-hamburgers/hamburgers.min.css">
+
+	<link rel="stylesheet" type="text/css" href="public/asset/select2/select2.min.css">
+
+    <link rel="stylesheet" type="text/css" href="public/css/register.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+   
+
+    <title>Register</title>
+</head>
+<body>
+<br>
+<div class="card" >
+		<div class="card-body">
+            <h1>ลงทะเบียน</h1>
+     
+			<form>
+				<div class="form-group">
+                    <label>อีเมล</label> <i class="fas fa-envelope"></i>
+                    <input  type="email" class="form-control"   placeholder="อีเมล">
+				</div>
+				<div class="form-group">
+					<label>รหัสผ่าน</label> <i class="fas fa-lock"></i>
+					<input type="password" class="form-control"  placeholder="รหัสผ่าน">
+				</div>
+                <div class="form-group">
+					<label>ยืนยันรหัสผ่าน</label> <i class="fas fa-lock"></i>
+					<input type="password" class="form-control"  placeholder="ยืนยันรหัสผ่าน">
+				</div>
+                <div class="form-group">
+					<label>เบอร์โทรศัพท์</label> <i class="fas fa-phone-square"></i>
+					<input type="text" class="form-control"  placeholder="เบอร์โทรศัพท์">
+				</div>
+			
+				
+				<button type="submit" class="btn btn-primary">ลงทะเบียน</button>
+				</a>
+            </form>
+        </div>
+	
+</div>
+    
+</body>
+</html>
