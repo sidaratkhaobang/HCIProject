5b1c1ec835ec16fa6224b158301b556060ce8338
Wichuta18 <slotsnow0301@gmail.com>
2019-04-07 23:20:47 +0700

    barcode

---
+++ application/views/barcode/barcode.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="http://localhost/HCIProject/public/css/barcode.css">
+   
+    <title>ชำระเงิน</title>
+</head>
+<body class= "bg">
+
+<h2>ชำระเงินร้านค้า</h2>
+<br>
+<div class="card" >
+		<div class="card-body">
+            <p>บาร์โค้ด</p><br>
+                <img src="public/img/barcode/barcode.png">
+        </div>
+</div>
+<br>
+<div class="card" >
+		<div class="card-body">
+            <p>คิวอาร์โค้ด</p><br>
+                <img src="public/img/barcode/QR.png">
+        </div>
+</div>
+
+</body>
+</html>
